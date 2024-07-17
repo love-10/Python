e6e18141d34751bdaf9d651d3f352158f77a721c
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-18 00:08:20 +0800

    1

---
+++ multi/const.py
@@ -0,0 +1,13 @@
+# 路径
+FILE_DIR = "./../data/labels"
+VIDEO_PATH = "./../data/testvideo1.mp4"
+VIDEO_OUTPUT_PATH = "./../data/multi-object.mp4"
+FPS = 30  # 帧率
+Size = (720, 360)
+
+GREEN = (0, 250, 0)
+RED = (0, 0, 250)
+
+COLOR_MEA = GREEN
+COLOR_STA = RED
+COLOR_MATCH = (255, 255, 0)
